541aff005f05f02da4dd39f6eaa56f9967b1c111
Sravan_T <129060926+sravan-11-git@users.noreply.github.com>
2023-10-12 13:00:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+function submitForm() {
+    
+    if (validateForm()) {
+        
+        displayPopup();
+        
+        resetForm();
+    }
+}
+
+function validateForm() {
+   
+    return true;
+}
+
+function resetForm() {
+    document.getElementById('surveyForm').reset();
+}
+
+function displayPopup() {
+    const popup = document.getElementById('popup');
+    const popupContent = document.getElementById('popupContent');
+
+    // Get form values
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const dob = document.getElementById('dob').value;
+    const country = document.getElementById('country').value;
+    const gender = Array.from(document.getElementsByName('gender'))
+        .filter(checkbox => checkbox.checked)
+        .map(checkbox => checkbox.value)
+        .join(', ');
+    const profession = document.getElementById('profession').value;
+    const email = document.getElementById('email').value;
+    const mobile = document.getElementById('mobile').value;
+
+    // Generate HTML content for the popup
+    const popupHTML = `
+        <p><strong>First Name:</strong> ${firstName}</p>
+        <p><strong>Last Name:</strong> ${lastName}</p>
+        <p><strong>Date of Birth:</strong> ${dob}</p>
+        <p><strong>Country:</strong> ${country}</p>
+        <p><strong>Gender:</strong> ${gender}</p>
+        <p><strong>Profession:</strong> ${profession}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Mobile Number:</strong> ${mobile}</p>
+    `;
+
+    popupContent.innerHTML = popupHTML;
+    popup.style.display = 'block';
+}
+
+function closePopup() {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'none';
+}
